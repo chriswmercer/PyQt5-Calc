dab49b871052c512b6c005b403934ae1760ef6af
Chris Mercer <chriswm@me.com>
2020-05-17 18:10:58 +0100

    Adding model and controller

--- pycalc.py
+++ pycalc.py
@@ -1,4 +1,6 @@
 import sys
+from functools import partial
+
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QWidget
@@ -7,6 +9,8 @@ from PyQt5.QtWidgets import QGridLayout
 from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QVBoxLayout
+
+ERR_MSG = "Error"
 
 
 class PyCalcUI(QMainWindow):
@@ -72,10 +76,50 @@ class PyCalcUI(QMainWindow):
         self.set_display_text("")
 
 
+class PyCalcController:
+    def __init__(self, model, view):
+        self._evaluate = model
+        self._view = view
+        self._connect_signals()
+
+    def _calculate_result(self):
+        result = self._evaluate(expression=self._view.display_text())
+        self._view.set_display_text(result)
+
+    def _connect_signals(self):
+        for button_text, button in self._view.buttons.items():
+            if button_text not in ("=", "C"):
+                button.clicked.connect(partial(self._build_expression, button_text))
+            elif button_text == "C":
+                button.clicked.connect(self._view.clear_display)
+            elif button_text == "=":
+                button.clicked.connect(self._calculate_result)
+        self._view.display.returnPressed.connect(partial(self._calculate_result))
+
+    def _build_expression(self, new_expression):
+        if self._view.display_text() == ERR_MSG:
+            self._view.clear_display()
+
+        expression = self._view.display_text() + new_expression
+        self._view.set_display_text(expression)
+
+
+def evaluate_expression(expression):
+    try:
+        result = str(eval(expression, {}, {}))
+    except Exception:
+        result = ERR_MSG
+    return result
+
+
 def main():
     pycalc = QApplication(sys.argv)
+
     view = PyCalcUI()
+    model = evaluate_expression
+    PyCalcController(model=model, view=view)
     view.show()
+
     sys.exit(pycalc.exec_())
 
 
